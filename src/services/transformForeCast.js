4a2dc2d9b62bb007a40d2b7538f2381287d47bbc
Jorge L <jorge.alfmur@gmail.com>
2018-02-25 23:10:56 -0500

    retornamos json con datos que necesitamos

--- src/services/transformForeCast.js
+++ src/services/transformForeCast.js
@@ -1,10 +1,19 @@
 import moment from 'moment';
+import 'moment/locale/es';
 
 const transformForeCast = (data) => (
   data.list.filter(item => (
     moment.unix(item.dt).utc().hour() == 6 ||
     moment.unix(item.dt).utc().hour() == 12 ||
     moment.unix(item.dt).utc().hour() == 18
+  )).map(item => (
+    {
+      weekDay: moment.unix(item.dt).format('dddd'),
+      hora: moment.unix(item.dt).utc().hour(),
+      temperatura: item.main.temp.toFixed(0) - 273,
+      humedad: item.main.humidity,
+      viento: item.wind.speed,
+    }
   ))
 );
 
